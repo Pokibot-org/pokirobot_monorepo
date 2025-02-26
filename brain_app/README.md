226cb952ab2debc8a903ba2cb26dc512aeb29841
Baptiste Cluzel <baptiste.clu@gmail.com>
2025-02-26 17:21:01 +0100

    Add pokutils lib

--- brain_app/README.md
+++ brain_app/README.md
@@ -5,3 +5,7 @@ The pc app should communicate with a program in python to visualize the robot ac
 
 This firmware is to be loaded on a weact mini_stm32h743 board.
 The board will be connected to several actuators, on lidar FHL-LD19, an ui board and a motor control board using a
+
+
+links:
+ - https://github.com/arirahikkala/astar-jps
---
+++ lib/pokutils/pokutils.c
@@ -0,0 +1,71 @@
+#include <math.h>
+#include <zephyr/kernel.h>
+#include <pokibot/lib/pokutils.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(pokutils);
+
+vec2_t point2_diff(const point2_t terminal, const point2_t initial)
+{
+    vec2_t delta = {
+        .dx = terminal.x - initial.x,
+        .dy = terminal.y - initial.y,
+    };
+    return delta;
+}
+
+vec2_t vec2_normalize(vec2_t vec)
+{
+    float length = vec2_abs(vec);
+    if (length > 0) {
+        vec.dx /= length;
+        vec.dy /= length;
+    }
+    return vec;
+}
+
+float vec2_abs(vec2_t vec)
+{
+    return sqrtf(vec.dx * vec.dx + vec.dy * vec.dy);
+}
+
+float vec2_dot(vec2_t a, vec2_t b)
+{
+    return a.dx * b.dx + a.dy * b.dy;
+}
+
+float vec2_angle(vec2_t a, vec2_t b)
+{
+    return acosf(vec2_dot(a, b) / (vec2_abs(a) * vec2_abs(b)));
+}
+
+float vec2_distance(point2_t a, point2_t b)
+{
+    return vec2_abs(point2_diff(a, b));
+}
+
+pos2_t pos2_diff(const pos2_t terminal, const pos2_t initial)
+{
+    pos2_t delta = {
+        .x = terminal.x - initial.x,
+        .y = terminal.y - initial.y,
+        .a = terminal.a - initial.a,
+    };
+    return delta;
+}
+
+pos2_t pos2_add(const pos2_t a, const pos2_t b)
+{
+    pos2_t c = {
+        .x = a.x + b.x,
+        .y = a.y + b.y,
+        .a = a.a + b.a,
+    };
+    return c;
+}
+
+
+float angle_modulo(float a)
+{
+    return fmodf(a + M_PI, 2 * M_PI) - M_PI;
+}
