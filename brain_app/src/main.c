70cff3e2498dc2f8a92eb92c4e819ebfc39675fd
bcluzel <baptiste.clu@gmail.com>
2025-03-02 22:07:19 +0100

    add some lines of code in main...

--- brain_app/src/main.c
+++ brain_app/src/main.c
@@ -1,10 +1,25 @@
+#include "pokibot/lib/poktocol.h"
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
+#include <pokibot/pokuicom.h>
 
 LOG_MODULE_REGISTER(main);
 
 int main(void)
 {
+    enum pokprotocol_team color;
+    while (!pokuicom_get_team_color(&color)) {
+        pokuicom_request(POKTOCOL_DATA_TYPE_TEAM);
+        k_sleep(K_MSEC(10));
+    }
+    LOG_INF("Team: %s", color == POKTOCOL_TEAM_BLUE ? "blue" : "yellow");
+
+    while (!pokuicom_is_match_started()) {
+        pokuicom_request(POKTOCOL_DATA_TYPE_MATCH_STARTED);
+        k_sleep(K_MSEC(10));
+    }
+
+    LOG_INF("Match started");
     k_sleep(K_FOREVER);
     return 0;
 }
