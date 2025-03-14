a25d574439a54f640e600edba06265ba6ebdb94c
Baptiste Cluzel <baptiste.clu@gmail.com>
2025-03-14 16:16:57 +0100

    Improve nav

--- brain_app/src/nav/nav.c
+++ brain_app/src/nav/nav.c
@@ -9,16 +9,19 @@
 LOG_MODULE_REGISTER(nav);
 
 K_EVENT_DEFINE(nav_events);
-K_THREAD_STACK_DEFINE(nav_thread_stack, 2048);
-static struct k_thread nav_thread;
+K_THREAD_STACK_DEFINE(nav_workq_stack, 2048);
+static struct k_work_q nav_workq;
+
+static void check_target_reached_work_handler(struct k_work *work);
+K_WORK_DELAYABLE_DEFINE(check_target_reached_work, check_target_reached_work_handler);
+
+static pos2_t target_pos;
 
 pos2_t sensivity = {
     .x = 0.01f,
     .y = 0.01f,
     .a = DEG_TO_RAD(2),
 };
-
-pos2_t target_pos = {-10000.0, -10000.0f};
 
 int nav_set_pos(const pos2_t *pos){
     return poklegscom_set_pos(pos);
@@ -30,6 +33,7 @@ int nav_go_to(const pos2_t *pos, k_timeout_t timeout)
     target_pos = *pos;
     poklegscom_set_break(0);
     poklegscom_set_waypoints(pos, 1);
+    k_work_schedule(&check_target_reached_work, K_NO_WAIT);
     return 0;
 }
 
@@ -38,24 +42,26 @@ void nav_wait_events(uint32_t *events)
     k_event_wait(&nav_events, 0xFFFFFFFF, false, K_FOREVER);
 }
 
-void nav_thread_task(void *arg0, void *arg1, void *arg2) {
-    while (1) {
-        pos2_t current_pos;
-        poklegscom_get_pos(&current_pos);
-        pos2_t diff = pos2_abs(pos2_diff(current_pos, target_pos));
-        if (POS2_COMPARE(diff, < , sensivity)) {
-            LOG_INF("Target reached");
-            k_event_set(&nav_events, NAV_EVENT_DESTINATION_REACHED);
-        }
-        k_sleep(K_MSEC(1000/25));
+static void check_target_reached_work_handler(struct k_work *work) {
+    pos2_t current_pos;
+    poklegscom_get_pos(&current_pos);
+    pos2_t diff = pos2_abs(pos2_diff(current_pos, target_pos));
+    if (POS2_COMPARE(diff, < , sensivity)) {
+        LOG_INF("Target reached");
+        k_event_set(&nav_events, NAV_EVENT_DESTINATION_REACHED);
+        return;
     }
+    k_work_reschedule_for_queue(&nav_workq, k_work_delayable_from_work(work), K_MSEC(1000/25));
 }
 
 int nav_init(void) {
+   	static struct k_work_queue_config cfg = {
+		.name = "nav_workq",
+	};
     poklegscom_set_break(1);
-    k_tid_t thread_id = k_thread_create(&nav_thread, nav_thread_stack, K_THREAD_STACK_SIZEOF(nav_thread_stack),
-										nav_thread_task, NULL, NULL, NULL, 10, 0, K_NO_WAIT);
-	k_thread_name_set(thread_id, "network");
+    k_work_queue_start(&nav_workq, nav_workq_stack,
+			   K_THREAD_STACK_SIZEOF(nav_workq_stack),
+			   8, &cfg);
 	return 0;
 }
 
