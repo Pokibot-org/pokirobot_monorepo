3bb6ae1609b22d91fe469f3769dac8e052718920
Baptiste Cluzel <baptiste.clu@gmail.com>
2025-05-19 21:23:50 +0200

    Moving from wact bord to custom interco board

---
+++ boards/pokibot/interco_weact_h743/board.cmake
@@ -0,0 +1,14 @@
+# SPDX-License-Identifier: Apache-2.0
+
+# keep first
+board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
+board_runner_args(pyocd "--target=stm32h743vitx")
+board_runner_args(dfu-util "--pid=0483:df11" "--alt=0" "--dfuse")
+board_runner_args(jlink "--device=STM32H743VI" "--speed=4000")
+
+# keep first
+include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/dfu-util.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
