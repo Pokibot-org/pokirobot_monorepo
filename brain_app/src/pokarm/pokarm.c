caaf8fa30a22dc1e5bf40da27d8440febd5c653a
Baptiste Cluzel <baptiste.clu@gmail.com>
2025-05-21 12:31:47 +0200

    Update interco hw description

--- brain_app/src/pokarm/pokarm.c
+++ brain_app/src/pokarm/pokarm.c
@@ -5,8 +5,10 @@
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(pokarm);
 
-// const struct device *stepper = DEVICE_DT_GET(DT_NODELABEL(stepper_arm));
-// const struct device *servo_deploy = DEVICE_DT_GET(DT_NODELABEL(servo_deploy_arm));
+const struct device *stepper = DEVICE_DT_GET(DT_NODELABEL(stepper_arm));
+const struct device *servo_deploy = DEVICE_DT_GET(DT_NODELABEL(servo_deploy_arm));
+const struct device *servo_left = DEVICE_DT_GET(DT_NODELABEL(servo_left_gripper));
+const struct device *servo_right = DEVICE_DT_GET(DT_NODELABEL(servo_right_gripper));
 
 
 void pokarm_reset_pos(void) {
