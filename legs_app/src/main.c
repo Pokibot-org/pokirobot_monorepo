f747f22e82e23696eb71c4ec490875d4a3d8cac6
bcluzel <baptiste.clu@gmail.com>
2025-05-26 12:53:39 +0200

    Is doing calibration angle

--- legs_app/src/main.c
+++ legs_app/src/main.c
@@ -5,13 +5,25 @@
 
 LOG_MODULE_REGISTER(main);
 
+void do_square(void) {
+    control_set_pos((pos2_t){.a = 0, .x = 0, .y = 0});
+    control_set_brake(false);
+
+    pos2_t src[] = {
+        (pos2_t){.a = 0.0f, .x = 200.0f, .y = 0.0f},
+        (pos2_t){.a = 0.0f, .x = 200.0f, .y = 200.0f},
+        (pos2_t){.a = 0.0f, .x = 0.0f, .y = 200.0f},
+        (pos2_t){.a = 0.0f, .x = 0.0f, .y = 0.0f},
+    };
+    control_set_waypoints(src, ARRAY_SIZE(src));
+}
+
 int main(void)
 {
     control_start();
     // com_start();
     // _test_motor_cmd();
     _test_calibration_angle();
-
-    k_sleep(K_FOREVER);
+    // do_square();
     return 0;
 }
