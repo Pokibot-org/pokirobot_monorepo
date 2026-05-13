f143a20761134dc592ede67bf23fda6ea57aa04c
bcluzel <baptiste.clu@gmail.com>
2026-05-13 19:11:00 +0200

    Update consts

--- brain_app/src/main.c
+++ brain_app/src/main.c
@@ -23,159 +23,34 @@ LOG_MODULE_REGISTER(main);
 #define STAND_DROP_DISTANCE_FROM_BORDER 0.075f
 
 enum nav_obstacle_names {
-    NAV_OBS_NAME_RAMPE1,
-    NAV_OBS_NAME_RAMPE2,
-    NAV_OBS_NAME_OBS_MIDDLE_LEFT,
-    NAV_OBS_NAME_OBS_MIDDLE_RIGHT,
-    NAV_OBS_NAME_OBS_RAMP_LEFT,
-    NAV_OBS_NAME_OBS_RAMP_RIGHT,
-    NAV_OBS_NAME_OBS_BOTOM_CENTER_LEFT,
-    NAV_OBS_NAME_OBS_BOTOM_CENTER_RIGHT,
-    NAV_OBS_NAME_OBS_BOTOM_CORNER_LEFT,
-    NAV_OBS_NAME_OBS_BOTOM_CORNER_RIGHT,
-    NAV_OBS_NAME_OBS_TOP_LEFT,
-    NAV_OBS_NAME_OBS_TOP_RIGHT,
+    NAV_OBS_NAME_GRENIER,
+    NAV_OBS_NAME_CRATES_TOP_RIGHT,
     NAV_OBS_NAME_LAST_STATIC_REGISTER,
 };
 
 // clang-format off
 struct nav_obstacle static_obstacles[] = {
     // RAMPE
-    [NAV_OBS_NAME_RAMPE1] = {
+    [NAV_OBS_NAME_GRENIER] = {
         .type = NAV_OBSTACLE_TYPE_RECTANGLE,
         .data.rectangle = {
             .point = {
                 .x = 0.0f,
-                .y = 1.90f,
+                .y = 1775.0f,
             },
-            .width = 1.7f,
-            .height = 0.2f,
+            .width = 1800.0f,
+            .height = 450.0f,
         }
     },
-    [NAV_OBS_NAME_RAMPE2] = {
+    [NAV_OBS_NAME_CRATES_TOP_RIGHT] = {
         .type = NAV_OBSTACLE_TYPE_RECTANGLE,
         .data.rectangle = {
             .point = {
-                .x = 0.0f,
-                .y = 1.65f,
+                .x = BOARD_MIN_X + 2825.0f,
+                .y = 1200.0f,
             },
-            .width = 0.9f,
-            .height = 0.3f,
-        }
-    },
-    // MIDDLE OBSTACLES
-    [NAV_OBS_NAME_OBS_MIDDLE_LEFT] = {
-        .type = NAV_OBSTACLE_TYPE_RECTANGLE,
-        .data.rectangle = {
-            .point = {
-                .x = -0.4f,
-                .y = 0.95f,
-            },
-            .width = 0.4f,
-            .height = 0.1f,
-        },
-    },
-    [NAV_OBS_NAME_OBS_MIDDLE_RIGHT] = {
-        .type = NAV_OBSTACLE_TYPE_RECTANGLE,
-        .data.rectangle = {
-            .point = {
-                .x = 0.4f,
-                .y = 0.95f,
-            },
-            .width = 0.4f,
-            .height = 0.1f,
-        }
-    },
-    // TOP CENTER
-    [NAV_OBS_NAME_OBS_RAMP_LEFT] = {
-        .type = NAV_OBSTACLE_TYPE_RECTANGLE,
-        .data.rectangle = {
-            .point = {
-                .x = BOARD_MIN_X + 0.825f,
-                .y = 1.725f,
-            },
-            .width = 0.4f,
-            .height = 0.1f,
-        }
-    },
-    [NAV_OBS_NAME_OBS_RAMP_RIGHT] = {
-        .type = NAV_OBSTACLE_TYPE_RECTANGLE,
-        .data.rectangle = {
-            .point = {
-                .x = BOARD_MAX_X - 0.825f,
-                .y = 1.725f,
-            },
-            .width = 0.4f,
-            .height = 0.1f,
-        }
-    },
-    // BOTTOM CENTER
-    [NAV_OBS_NAME_OBS_BOTOM_CENTER_LEFT] = {
-        .type = NAV_OBSTACLE_TYPE_RECTANGLE,
-        .data.rectangle = {
-            .point = {
-                .x = BOARD_MIN_X + 0.775f,
-                .y = 0.25f,
-            },
-            .width = 0.4f,
-            .height = 0.1f,
-        }
-    },
-    [NAV_OBS_NAME_OBS_BOTOM_CENTER_RIGHT] = {
-        .type = NAV_OBSTACLE_TYPE_RECTANGLE,
-        .data.rectangle = {
-            .point = {
-                .x = BOARD_MAX_X - 0.775f,
-                .y = 0.25f,
-            },
-            .width = 0.4f,
-            .height = 0.1f,
-        }
-    },
-    // BOTTOM LR
-    [NAV_OBS_NAME_OBS_BOTOM_CORNER_LEFT] = {
-        .type = NAV_OBSTACLE_TYPE_RECTANGLE,
-        .data.rectangle = {
-            .point = {
-                .x = BOARD_MIN_X + 0.075f,
-                .y = 0.4f,
-            },
-            .width = 0.1f,
-            .height = 0.4f,
-        }
-    },
-    [NAV_OBS_NAME_OBS_BOTOM_CORNER_RIGHT] = {
-        .type = NAV_OBSTACLE_TYPE_RECTANGLE,
-        .data.rectangle = {
-            .point = {
-                .x = BOARD_MAX_X - 0.075f,
-                .y = 0.4f,
-            },
-            .width = 0.1f,
-            .height = 0.4f,
-        }
-    },
-    // TOP LR
-    [NAV_OBS_NAME_OBS_TOP_LEFT] = {
-        .type = NAV_OBSTACLE_TYPE_RECTANGLE,
-        .data.rectangle = {
-            .point = {
-                .x = BOARD_MIN_X + 0.075f,
-                .y = 1.325f,
-            },
-            .width = 0.1f,
-            .height = 0.4f,
-        }
-    },
-    [NAV_OBS_NAME_OBS_TOP_RIGHT] = {
-        .type = NAV_OBSTACLE_TYPE_RECTANGLE,
-        .data.rectangle = {
-            .point = {
-                .x = BOARD_MAX_X - 0.075f,
-                .y = 1.325f,
-            },
-            .width = 0.1f,
-            .height = 0.4f,
+            .width = 150.0f,
+            .height = 150.0f,
         }
     },
 };
@@ -189,65 +64,20 @@ K_WORK_DELAYABLE_DEFINE(end_of_match_work, end_of_match_work_handler);
 
 // DEFINE EVERYTHING FOR BLUE
 
-const struct nav_obstacle end_zone = {
-    .type = NAV_OBSTACLE_TYPE_RECTANGLE,
-    .data.rectangle = {
-        .height = 0.45f,
-        .width = 0.45f,
-        .point = {
-            .x = BOARD_MIN_X + 2.625f,
-            .y = 1.775f
-        }
-    }
-};
-
-const struct nav_obstacle left_zone = {
-    .type = NAV_OBSTACLE_TYPE_RECTANGLE,
-    .data.rectangle = {
-        .height = 0.45f,
-        .width = 0.45f,
-        .point = {
-            .x = BOARD_MIN_X + 0.225f,
-            .y = 0.875f
-        }
-    }
-};
-
 const struct nav_obstacle start_zone = {
     .type = NAV_OBSTACLE_TYPE_RECTANGLE,
     .data.rectangle = {
-        .height = 0.45f,
-        .width = 0.45f,
         .point = {
-            .x = BOARD_MIN_X + 1.775f,
-            .y = 0.225f
-        }
+            .x = BOARD_MIN_X + 2700.0f,
+            .y = 1800.0f
+        },
+        .height = 450.0f,
+        .width = 600.0f,
     }
 };
 
-const struct nav_obstacle left_corner_tiny_zone = {
-    .type = NAV_OBSTACLE_TYPE_RECTANGLE,
-    .data.rectangle = {
-        .height = 0.15f,
-        .width = 0.45f,
-        .point = {
-            .x = BOARD_MIN_X + 0.225f,
-            .y = 0.0775f
-        }
-    }
-};
+const struct nav_obstacle end_zone = start_zone;
 
-const struct nav_obstacle center_tiny_zone = {
-    .type = NAV_OBSTACLE_TYPE_RECTANGLE,
-    .data.rectangle = {
-        .height = 0.15f,
-        .width = 0.45f,
-        .point = {
-            .x = BOARD_MIN_X + 2.225f,
-            .y = 0.0775f
-        }
-    }
-};
 
 struct point2 convert_point_for_team(enum pokprotocol_team color, struct point2 point)
 {
@@ -307,53 +137,6 @@ uint8_t score = 0;
         return -1;                                                                                 \
     }
 
-int start_second_target(enum pokprotocol_team color)
-{
-    const float consigne_a = - ROBOT_ARM_ANGLE_OFFSET;
-    uint32_t nav_events;
-
-    LOG_INF("SECOND TARGET");
-    const struct nav_obstacle_rectangle cor = static_obstacles[NAV_OBS_NAME_OBS_MIDDLE_RIGHT].data.rectangle;
-    const pos2_t wp2_1 = {
-        .x = cor.point.x + cor.width / 2 + ROBOT_RADIUS + 0.05f,
-        .y = cor.point.y,
-        .a = -M_PI/2
-    };
-
-    pos2_t wp2_2 = wp2_1;
-    wp2_2.y += ROBOT_RADIUS + 0.20f;
-    pos2_t wp2_3 = wp2_2;
-    wp2_3.x = cor.point.x;
-    pos2_t wp2_4 = CONVERT_POINT2_TO_POS2(cor.point, -M_PI/2);
-    pos2_t wp2_5 = CONVERT_POINT2_TO_POS2(start_zone.data.rectangle.point, -M_PI/2);
-    pos2_t wp2_6 = CONVERT_POINT2_TO_POS2(start_zone.data.rectangle.point, -M_PI/2);
-    wp2_6.y = 0.6f;
-
-    k_timeout_t per_task_timeout = K_SECONDS(15);
-
-    nav_go_to_direct(convert_pos_for_team(color, wp2_1, consigne_a), per_task_timeout);
-    WAIT_EVENTS_AND_HANDLE(nav_events);
-    nav_go_to_direct(convert_pos_for_team(color, wp2_2, consigne_a), per_task_timeout);
-    WAIT_EVENTS_AND_HANDLE(nav_events);
-    nav_go_to_direct(convert_pos_for_team(color, wp2_3, consigne_a), per_task_timeout);
-    WAIT_EVENTS_AND_HANDLE(nav_events);
-    pokarm_deploy(true);
-    nav_set_speed(200.0f, NAV_ANGULAR_VMAX_DEFAULT);
-    nav_go_to_direct(convert_pos_for_team(color, wp2_4, consigne_a), per_task_timeout);
-    nav_wait_events(&nav_events);
-    nav_go_to_direct(convert_pos_for_team(color, wp2_5, consigne_a), per_task_timeout);
-    nav_wait_events(&nav_events);
-    score += STRAT_STAND_SCORE_LV1;
-    pokuicom_send_score(score);
-    nav_go_to_direct(convert_pos_for_team(color, wp2_6, consigne_a), per_task_timeout);
-    nav_wait_events(&nav_events);
-
-    pokarm_deploy(false);
-    nav_set_speed(400.0f, NAV_ANGULAR_VMAX_DEFAULT);
-    LOG_INF("SECOND TARGET OK");
-    return 0;
-}
-
 int match(enum pokprotocol_team color)
 {
     uint32_t match_start_time_ms =  k_uptime_get_32();
@@ -400,11 +183,10 @@ int match(enum pokprotocol_team color)
     LOG_INF("FIRST TARGET OK");
 
     pokarm_deploy(false);
-    nav_set_speed(400.0f, NAV_ANGULAR_VMAX_DEFAULT);
+    nav_set_speed(NAV_PLANAR_VMAX_DEFAULT, NAV_ANGULAR_VMAX_DEFAULT);
 
-    start_second_target(color);
 
-    LOG_INF("GO BACKSTAGE");
+    LOG_INF("GO HOME");
     const pos2_t end_pos = CONVERT_POINT2_TO_POS2(end_zone.data.rectangle.point, -M_PI/2);
     const pos2_t wpbs_0 = {.x = BOARD_MIN_X + 2.225f, .y = 0.6f, .a = -M_PI/2 };
     pos2_t wp0_wait_before_backstage = {
@@ -452,7 +234,7 @@ int match(enum pokprotocol_team color)
 
 void test_pomicontrol(void) {
     while (1) {
-        pomicontrol_activate();
+        // pomicontrol_activate();
         k_sleep(K_MSEC(1000));
     }
 }
